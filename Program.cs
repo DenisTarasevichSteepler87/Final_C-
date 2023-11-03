d0f75d83d9dd3b64f3d420a1440040a453e31850
Denis <9544380@mail.ru>
2023-11-03 17:28:44 +0300

    написание программы решения задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array1 = new string[n];
+
+void InputArray(string[] array)
+{
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write("Введите элемент №{0}:\r\n    ", i + 1);
+        array1[i] = Console.ReadLine();
+    }
+}
+
+
+void FillArray(string[] array, string[] arrayFill)
+{
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i].Length <= 3)
+        {
+            arrayFill[j] = array[i];
+            j++;
+        }
+
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array1.Length; i++)
+        System.Console.Write($"{array[i]} ");
+    System.Console.WriteLine();
+}
+
+string[] array2 = new string[array1.Length];
+InputArray(array1);
+System.Console.WriteLine("Введенный массив:");
+PrintArray(array1);
+FillArray(array1, array2);
+System.Console.WriteLine("Полученный массив:");
+PrintArray(array2);
+
